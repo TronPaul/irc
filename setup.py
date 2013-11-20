6e921ba7183bbb39df9d88afbab89af5addf3d1a
Mark McGuire <mark.b.mcg@gmail.com>
2013-11-19 21:29:32 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='irc',
+    version='0.0.1',
+    packages=['irc'],
+    url='https://github.com/TronPaul/irc',
+    license='',
+    author='Mark McGuire',
+    author_email='',
+    description='Irc library using aysncio'
+)
